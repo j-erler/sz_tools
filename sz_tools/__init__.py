221d95685f02a144f83c45b51af0c00920a816d6
j-erler <35317736+j-erler@users.noreply.github.com>
2018-12-04 13:44:15 +0100

    Update __init__.py

--- sz_tools/__init__.py
+++ sz_tools/__init__.py
@@ -1,28 +1,34 @@
+
+# -*- coding: utf-8 -*-
 # 
-#  This file is part of Healpy.
+#  This file is part of sz_tools.
 # 
-#  Healpy is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
+#  sz_tools is free software; you can redistribute it and/or modify
+#  it under the terms of the MIT License.
 # 
-#  Healpy is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-# 
-#  You should have received a copy of the GNU General Public License
-#  along with Healpy; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
-#  For more information about Healpy, see http://code.google.com/p/healpy
-# 
-"""HealPy is a package to manipulate Healpix maps (ang2pix, pix2ang) and
-compute spherical harmonics tranforms on them.
+#  sz_tools is distributed in the hope that it will be useful,but 
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See 
+#  the provided copy of the MIT License for more details.
+
+"""sz_tools is a python implementation of matched filtering techniques for 
+   applications in astronomy.
 """
 
-#from .version import __version__
+__version__ = "1.0"
 
-from .sz_tools import (planck_uc, planck_beams, tsz_spec, tsz_spec_planck, ksz_spec, m500_2_r500, r500_2_m500, beta_model, beta_projected, gnfw, gnfw_projected, gnfw_projected_fast, gnfw_abel, simulate_cluster, simulate_cluster_beta, deproject, Y_500_sph, Y_500_cyl, T_e_profile, T_sz, T_sz_fast, tau_fast, simulate_rel_cluster, Y_500_planck, M_500_planck)
-from .shortcuts import (make_table, write_file, read_file, writefits, readfits, hubble, angular_dist, luminosity_dist, dist, gaussian, create_histogram, convert_units, mbb_spec, cov2corr, corr2cov, compact_error, quantile, find_level)
-from .ilc import (create_header, project_maps, ilc_windows, remove_offset, run_ilc, ilc_scales, ilc, ilc_allsky)
+__bibtex__ = """
+"""
+
+from .sz_tools import (planck_uc, planck_beams, tsz_spec, tsz_spec_planck, ksz_spec, 
+                       m500_2_r500, r500_2_m500, beta_model, beta_projected, gnfw, 
+                       gnfw_projected, gnfw_projected_fast, gnfw_abel, simulate_cluster, 
+                       simulate_cluster_beta, deproject, Y_500_sph, Y_500_cyl, T_e_profile, 
+                       T_sz, T_sz_fast, tau_fast, simulate_rel_cluster, Y_500_planck, 
+                       M_500_planck)
+from .shortcuts import (make_table, write_file, read_file, writefits, readfits, hubble, 
+                        angular_dist, luminosity_dist, dist, gaussian, create_histogram, 
+                        convert_units, mbb_spec, cov2corr, corr2cov, compact_error, quantile, 
+                        find_level)
+from .ilc import (create_header, project_maps, ilc_windows, remove_offset, run_ilc, ilc_scales, 
+                  ilc, ilc_allsky)
