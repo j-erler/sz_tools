7af732568ec9961b42664d8f31560e5cad37a0e2
j-erler <35317736+j-erler@users.noreply.github.com>
2021-01-05 00:57:32 +0100

    Update __init__.py

--- sz_tools/__init__.py
+++ sz_tools/__init__.py
@@ -25,10 +25,10 @@ from .sz_tools import (planck_uc, planck_beams, tsz_spec, tsz_spec_planck, ksz_s
                        gnfw_projected, gnfw_projected_fast, gnfw_abel, simulate_cluster, 
                        simulate_cluster_beta, deproject, Y_500_sph, Y_500_cyl, T_e_profile, 
                        T_sz, T_sz_fast, tau_fast, simulate_rel_cluster, Y_500_planck, 
-                       M_500_planck, r200r500, m200m500)
+                       M_500_planck, r200r500, m200m500, rebin)
 from .shortcuts import (make_table, write_file, read_file, writefits, readfits, hubble, 
                         angular_dist, luminosity_dist, dist, gaussian, create_histogram, 
                         convert_units, mbb_spec, cov2corr, corr2cov, compact_error, quantile, 
-                        find_level, angle, radial_profile, rebin, sample_sphere_uniform)
+                        find_level, angle, radial_profile, sample_sphere_uniform)
 from .ilc import (create_header, project_maps, ilc_windows, remove_offset, run_ilc, ilc_scales, 
                   ilc, ilc_allsky)
